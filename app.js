94e0feb6b4f77043113727ec1b96dc4d605dcc9d
mohamed mahmoud jedeine <moud@mohameds-MacBook-Air.local>
2017-10-24 20:55:47 -0400

    my commit

---
+++ app.js
@@ -0,0 +1,60 @@
+// import my inquires
+var myimports=require('./word.js');
+var inquirer=require('inquirer');
+var Word=myimports.word;
+var Letter=myimports.letters;
+// *****************************//
+console.log("Welcome to My Hangman!");
+console.log("Guess the name of the city");
+console.log("Goodluck!");
+// create a function constructor for game
+var game=function(){
+// initiate the game
+var guessremain=10;
+// generate a random word
+var wordtoguess=myimports.random();
+var word_=new Word(wordtoguess);
+// init the word _
+word_.init();
+// and diplay it
+console.log(word_.displayword());
+// intern function for a game cycle
+function gamecycle() {
+// to loop through the prompt
+if(guessremain>0)
+{
+// remainig guesses	
+console.log('You have '+guessremain+' Guesses remaining');
+inquirer.prompt([
+  {
+    name: "letter",
+    message: "choose a letter :"
+  }
+]).then(function(answers) {
+    // check the letter  
+    word_.check(answers.letter);
+    // display the result
+    console.log(word_.displayword());
+    // decrement the remaining guesses
+    guessremain--;
+    // play it again
+   gamecycle();
+   // if all letters in the word were guessed the player win and a new game will be generated
+   if(word_.end())
+   {
+   	console.log('-----------------you won---------------------');
+   	console.log('a new city name will be generated');
+   	new game();
+   }
+});
+}
+else{
+    // if the player has tried 10 times  a new game will be generated
+	console.log('------------------you lost------------------');
+	console.log('a new city name will be generated');
+	new game();
+}
+}
+gamecycle();
+}
+new game();
---
+++ word.js
@@ -0,0 +1,72 @@
+// table of city names 
+var  table_words=['barcelona','washington','paris','delhi','khartoum'];
+// function to generate a random name 
+function random()
+{
+this.random_word=table_words[Math.floor(Math.random()*table_words.length-1)];
+return this.random_word;
+}
+// function constructor for words 	
+function word(word)
+{
+this.word=word;
+this.word_=[];
+// a method to initiat the word with _
+this.init=function(){
+for (var i = 0; i<this.word.length; i++) {
+	this.word_.push(new letters(this.word[i]));
+}
+}
+// to display the word as a string
+this.displayword=function(){
+	var interword='';
+    for (var i = 0; i < this.word_.length; i++) {
+    		interword=interword+this.word_[i].display();
+    	}	
+  return interword;  	
+}
+// to check if a letter is in a word
+this.check=function(lettertocheck){
+	var checkresult;
+  for (var i = 0; i < this.word_.length; i++) {
+  	  if(this.word_[i].letter===lettertocheck && this.word_[i].guessedletter==false)
+  	  {
+       this.word_[i].guessedletter=true;
+       checkresult='correct';
+       break;
+  	  }else
+  	  {
+  	  	checkresult='incorrect';
+  	  }
+  }
+  console.log(checkresult);
+}
+// to check if there still letters to check or the word was guessed
+this.end=function(){
+   var end = true;
+	for (var i = 0; i < this.word_.length; i++) {
+         if(this.word_[i].guessedletter===false)
+         {
+          end=false;
+          break;
+         }
+	}
+	return end;
+}
+}
+// to construct a letter that will display _ if it's not guessed and the letter if it's not the case
+function letters(letter)
+{
+this.letter=letter;
+this.guessedletter=false;
+this.display= function() {
+	 if(this.guessedletter)
+ {
+ 	return this.letter;
+ }
+ else
+ 	return '_';
+}
+}
+// export those three constructo functions
+module.exports={word,letters,random};
